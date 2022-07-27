1a575de64a8df4b794f53366ae156687ce49dad2
Niilo Lappi <thenipanen@DESKTOP-S5RVM6C.localdomain>
2022-07-28 01:02:18 +0300

    Part 6 d

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,23 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = ''
+const initialState = ['', -1]
 
 const notificationSlice = createSlice({
   name: 'notification',
   initialState,
   reducers: {
     set(state, action) {
-      return action.payload
+      return [action.payload, state[1]]
+    },
+    clearPrev(state, action) {
+      clearTimeout(state[1])
+      return [state[0], action.payload]
     }
   }
 })
 
-const { set } = notificationSlice.actions
+const { set, clearPrev } = notificationSlice.actions
 
 export const setNotification = (message, seconds) => {
   return dispatch => {
     dispatch(set(message))
-    setTimeout(() => dispatch(set('')), seconds * 1000)
+    const next = setTimeout(() => dispatch(set('')), seconds * 1000)
+    dispatch(clearPrev(next))
   }
 }
 
